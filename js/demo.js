34acfeef9213b0dee15da58b992779faf5064b84
Ajay Jain <ajay.jain@travelyaari.com>
2016-09-02 12:28:37 -0700

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -1,8 +1,3 @@
-/**
- * Particleground demo
- * @author Jonathan Nicol - @mrjnicol
- */
-
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
     dotColor: '#d3d3d3',
@@ -11,17 +6,29 @@ document.addEventListener('DOMContentLoaded', function () {
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
-
-
-/*
-// jQuery plugin example:
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
-*/
+if (document.layers) {
+    //Capture the MouseDown event.
+    document.captureEvents(Event.MOUSEDOWN);
+ 
+    //Disable the OnMouseDown event handler.
+    document.onmousedown = function () {
+        return false;
+    };
+}
+else {
+    //Disable the OnMouseUp event handler.
+    document.onmouseup = function (e) {
+        if (e != null && e.type == "mouseup") {
+            //Check the Mouse Button which is clicked.
+            if (e.which == 2 || e.which == 3) {
+                //If the Button is middle or right then disable.
+                return false;
+            }
+        }
+    };
+}
+ 
+//Disable the Context Menu event.
+document.oncontextmenu = function () {
+    return false;
+};
